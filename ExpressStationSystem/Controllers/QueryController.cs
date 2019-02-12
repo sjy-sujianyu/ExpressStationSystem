e62de5c2a62dd0b817cdc17cb9e3b858caf85a49
sujianyu1997 <1040575641@qq.com>
2019-02-12 19:02:27 +0800

    新增包裹查询全部信息

--- ExpressStationSystem/Controllers/QueryController.cs
+++ ExpressStationSystem/Controllers/QueryController.cs
@@ -20,11 +20,12 @@ namespace ExpressStationSystem.Controllers
         /// <remarks>根据包裹得到物流信息</remarks>
         /// <returns>返回</returns>
         [HttpGet, Route("Query/GetLogisticsInfo")]
-        public List<dynamic> GetLogisticsInfo(int id)
+        private List<dynamic> GetLogisticsInfo(int id)
         {
             db = new DataClasses1DataContext(connstr);
-            var selectQuery = from a in db.Path join b in db.Branch on a.curId equals b.bId where a.id==id select new { path = a, branch = b };
+            var selectQuery = from a in db.Path join b in db.Branch on a.curId equals b.bId join c in db.Vehicle on a.vId equals c.vId where a.id==id select new { path = a, branch = b,vehicle=c };
             List<dynamic> list = new List<dynamic>();
+
             foreach (var x in selectQuery)
             {
                 list.Add(x);
@@ -32,59 +33,30 @@ namespace ExpressStationSystem.Controllers
             return list;
         }
 
-        //// GET: api/Query/GetStatus?account={account}
-        ///// <summary>
-        ///// 得到包裹状态信息
-        ///// </summary>
-        ///// <param name="id">包裹ID</param>
-        ///// <remarks>得到包裹状态信息</remarks>
-        ///// <returns>返回</returns>
-        //[HttpGet, Route("Query/GetStatus")]
-        //public string GetStatus(int id)
-        //{
-        //    db = new DataClasses1DataContext(connstr);
-        //    var isTransport = from a in db.Path where a.id == id select a;
-        //    if (isTransport.Count() == 0)
-        //    {
-        //        var isReady = from a in db.PickUp where a.id == id select a;
-        //        if (isReady.Count() == 0)
-        //        {
-        //            return "待揽收";
-        //        }
-        //        else
-        //        {
-        //            return "正在揽收";
-        //        }
-
-        //    }
-        //    else if(isTransport.Count()==1)
-        //    {
-        //        return "已揽收";
-        //    }
-        //    else
-        //    {
-        //        var isSignIn = from a in db.Delivery where a.id == id select a;
-        //        if (isSignIn.Count() == 0)
-        //        {
-        //            return "运输中";
-        //        }
-        //        else
-        //        {
-
-        //            foreach(var x in isSignIn)
-        //            {
-        //                return x.status;
-        //            }
-        //        }
-        //    }
-        //    //var selectQuery = from a in db.Path join b in db.Branch on a.curId equals b.bId where a.id == id select new { path = a, branch = b };
-        //    //List<dynamic> list = new List<dynamic>();
-        //    //foreach (var x in selectQuery)
-        //    //{
-        //    //    list.Add(x);
-        //    //}
-        //    //return list;
-        //}
+        // GET: api/Query/GetAllInfo?account={account}
+        /// <summary>
+        /// 得到包裹全部信息
+        /// </summary>
+        /// <param name="id">包裹ID</param>
+        /// <remarks>得到包裹全部信息</remarks>
+        /// <returns>返回</returns>
+        [HttpGet, Route("Query/GetAllInfo")]
+        public dynamic GetAllInfo(int id)
+        {
+            db = new DataClasses1DataContext(connstr);
+            var package = db.Package.SingleOrDefault(a => a.id == id);
+            if(package is null)
+            {
+                return null;
+            }
+            var pickup = db.PickUp.SingleOrDefault(a => a.id == id);
+            var src = db.AddressBook.SingleOrDefault(a => a.aId == package.sendId);
+            var dest = db.AddressBook.SingleOrDefault(a => a.aId == package.receiverId);
+            var delivery = db.Delivery.SingleOrDefault(a => a.id == id);
+            var transfer = db.Transfer.SingleOrDefault(a => a.id == id);
+            var list=GetLogisticsInfo(id);
+            return new { package = package, pickup = pickup, src = src, dest = dest, delivery = delivery, transfer = transfer,pathList=list };
+        }
         // POST: api/Query
         public void Post([FromBody]string value)
         {
