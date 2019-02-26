f1b985562712415ae22ebea682364e3ebf7294ee
YangQZhou <240499077@qq.com>
2019-02-26 12:35:19 +0800

    经理网页

---
+++ ExpressStationSystem/Controllers/ViewController/LogInController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExpressStationSystem.Controllers.ViewController
+{
+    public class LogInController : Controller
+    {
+        // GET: LogIn
+        public ActionResult AfterLogin()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult changePassword()
+        {
+            return View();
+        }
+    }
+}
---
+++ ExpressStationSystem/Controllers/ViewController/PackageController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExpressStationSystem.Controllers.ViewController
+{
+    public class PackageController : Controller
+    {
+        public ActionResult Package()
+        {
+            return View();
+        }
+    }
+}
