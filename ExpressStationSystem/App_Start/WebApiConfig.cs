f7a9e320ec5af18e3936758584efe06d17f15410
sujianyu1997 <1040575641@qq.com>
2019-03-01 19:23:57 +0800

    新增跨域问题解决组件

--- ExpressStationSystem/App_Start/WebApiConfig.cs
+++ ExpressStationSystem/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace ExpressStationSystem
 {
@@ -9,6 +10,7 @@ namespace ExpressStationSystem
     {
         public static void Register(HttpConfiguration config)
         {
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
             // Web API 配置和服务
 
             // Web API 路由
