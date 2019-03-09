d4b976676c936f6c1545b76248bc9cde2d431c08
YangQZhou <240499077@qq.com>
2019-03-09 21:21:22 +0800

    基本完成

--- ExpressStationSystem/Controllers/ViewController/PackageController.cs
+++ ExpressStationSystem/Controllers/ViewController/PackageController.cs
@@ -4,188 +4,282 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ExpressStationSystem.Controllers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace ExpressStationSystem.Controllers.ViewController
 {
     public class PackageController : Controller
     {
-        List<int> PIDList = new List<int>();
-        List<string> nameList = new List<string>();
-        List<string> phoneList = new List<string>();
-        List<DateTime> timeList = new List<DateTime>();
-        List<string> statusList = new List<string>();
-        List<int> errorStatusList = new List<int>();
-
-        public ActionResult Package(string status, string searchWithName, string searchWithPhone, string searchWithPID, string searchWithTime)
+        public ActionResult Package(string status, string searchWith, string searchWithContent)
         {
-            nameList.Clear();
-            phoneList.Clear();
-            timeList.Clear();
-            statusList.Clear();
+            string defaultSearchWith = "按姓名";
+            string defaultSearchWithContent = "";
+            string defaultStatus = "选择分类";
 
-            //获取最新的包裹列表
-            List<int> packagesID = new ManagerController().GetAllPackage();
-
-            foreach (var one in packagesID)
+            if (status == "undefined" || status == null)
             {
-                var packageInfo = new QueryController().GetAllInfo(one);
-                //包裹状态
-                string packageStatus = packageInfo.package.status;
-                if (status != null)
+                status = defaultStatus;
+                ViewBag.status = status;
+            }
+            else
+            {
+                ViewBag.status = status;
+            }
+            if (searchWith == null)
+            {
+                ViewBag.searchWith = defaultSearchWith;
+            }
+            else
+            {
+                ViewBag.searchWith = searchWith;
+            }
+            if (searchWithContent == null)
+            {
+                ViewBag.searchWithContent = defaultSearchWithContent;
+            }
+            else
+            {
+                ViewBag.searchWithContent = searchWithContent;
+            }
+            //人员信息
+            List<int> PIDList = new ManagerController().GetAllPackage();
+            //中间数组
+            List<dynamic> step = new List<dynamic>();
+            //决定显示的前端数组
+            List<dynamic> showPackage = new List<dynamic>();
+            //第一次筛选
+            foreach (var PID in PIDList)
+            {
+                var PackageInfo = new QueryController().GetAllInfo(PID);
+                var josnStr = JsonConvert.SerializeObject(PackageInfo);
+                JObject jo = (JObject)JsonConvert.DeserializeObject(josnStr);
+
+                if (status == PackageInfo.package.status)
                 {
-                    if (packageInfo.error == null)
-                    {
-                        errorStatusList.Add(0);
-                    }
-                    else
-                    {
-                        errorStatusList.Add(1);
-                    }
-                    //状态相同就添加数组
-                    if(packageStatus == status)
-                    {
-                        PIDList.Add(one);
-
-                        statusList.Add(packageStatus);
-
-                        nameList.Add(packageInfo.dest.name);
-
-                        phoneList.Add(packageInfo.dest.phone);
-
-                        timeList.Add(packageInfo.package.time);
-                    }
+                    step.Add(jo);
                     continue;
                 }
-                else if(searchWithName != null)
+                else if(PackageInfo.error != null && status == PackageInfo.error.status)
                 {
-                    if(packageInfo.dest.name.ToString().StartsWith(searchWithName))
-                    {
-                        if (packageInfo.error == null)
-                        {
-                            errorStatusList.Add(0);
-                        }
-                        else
-                        {
-                            errorStatusList.Add(1);
-                        }
-                        PIDList.Add(one);
-
-                        statusList.Add(packageInfo.package.status);
-
-                        nameList.Add(packageInfo.dest.name);
-
-                        phoneList.Add(packageInfo.dest.phone);
-
-                        timeList.Add(packageInfo.package.time);
-                    }
+                    step.Add(jo);
                     continue;
                 }
-                else if(searchWithPhone != null)
+                else if (status == defaultStatus)
                 {
-                    if (packageInfo.dest.phone.ToString().StartsWith(searchWithPhone))
-                    {
-                        if (packageInfo.error == null)
-                        {
-                            errorStatusList.Add(0);
-                        }
-                        else
-                        {
-                            errorStatusList.Add(1);
-                        }
-
-                        PIDList.Add(one);
-
-                        statusList.Add(packageInfo.package.status);
-
-                        nameList.Add(packageInfo.dest.name);
-
-                        phoneList.Add(packageInfo.dest.phone);
-
-                        timeList.Add(packageInfo.package.time);
-                    }
+                    step.Add(jo);
                     continue;
                 }
-                else if (searchWithPID != null)
+            }
+
+            foreach (var PInfo in step)
+            {
+                if (searchWith != null)
                 {
-                    if (one.ToString().ToString().StartsWith(searchWithPID))
+                    if (searchWith == "按单号")
                     {
-                        if (packageInfo.error == null)
+                        if (PInfo.package.id.ToString().StartsWith(searchWithContent))
                         {
-                            errorStatusList.Add(0);
+                            showPackage.Add(PInfo);
                         }
-                        else
-                        {
-                            errorStatusList.Add(1);
-                        }
-                        PIDList.Add(one);
-
-                        statusList.Add(packageInfo.package.status);
-
-                        nameList.Add(packageInfo.dest.name);
-
-                        phoneList.Add(packageInfo.dest.phone);
-
-                        timeList.Add(packageInfo.package.time);
                     }
-                    continue;
-                }
-                else if (searchWithTime != null)
-                {
-                    
-                    if (packageInfo.package.time.ToString().StartsWith(searchWithTime))
+                    else if (searchWith == "按姓名")
                     {
-                        if (packageInfo.error == null)
+                        if (PInfo.dest.name.ToString().StartsWith(searchWithContent))
                         {
-                            errorStatusList.Add(0);
+                            showPackage.Add(PInfo);
                         }
-                        else
-                        {
-                            errorStatusList.Add(1);
-                        }
-                        PIDList.Add(one);
-
-                        statusList.Add(packageInfo.package.status);
-
-                        nameList.Add(packageInfo.dest.name);
-
-                        phoneList.Add(packageInfo.dest.phone);
-
-                        timeList.Add(packageInfo.package.time);
                     }
-                    continue;
+                    else if (searchWith == "按手机")
+                    {
+                        if (PInfo.dest.phone.ToString().StartsWith(searchWithContent))
+                        {
+                            showPackage.Add(PInfo);
+                        }
+                    }
+                    else if (searchWith == "按时间")
+                    {
+                        if (PInfo.package.time.ToString().StartsWith(searchWithContent))
+                        {
+                            showPackage.Add(PInfo);
+                        }
+                    }
                 }
                 else
                 {
-                    if (packageInfo.error == null)
-                    {
-                        errorStatusList.Add(0);
-                    }
-                    else
-                    {
-                        errorStatusList.Add(1);
-                    }
-                    PIDList.Add(one);
-
-                    statusList.Add(packageInfo.package.status);
-
-                    nameList.Add(packageInfo.dest.name);
-
-                    phoneList.Add(packageInfo.dest.phone);
-
-                    timeList.Add(packageInfo.package.time);
-
-                    continue;
+                    showPackage.Add(PInfo);
                 }
-                
             }
-
-            ViewBag.errorStatusList = errorStatusList;
-            ViewBag.packagesID = PIDList;
-            ViewBag.nameList = nameList;
-            ViewBag.phoneList = phoneList;
-            ViewBag.timeList = timeList;
-            ViewBag.statusList = statusList;
+            ViewBag.showPackage = showPackage;
             return View();
+            //nameList.Clear();
+            //phoneList.Clear();
+            //timeList.Clear();
+            //statusList.Clear();
+
+            ////获取最新的包裹列表
+            //List<int> packagesID = new ManagerController().GetAllPackage();
+
+            //foreach (var one in packagesID)
+            //{
+            //    var packageInfo = new QueryController().GetAllInfo(one);
+            //    //包裹状态
+            //    string packageStatus = packageInfo.package.status;
+            //    if (status != null)
+            //    {
+            //        if (packageInfo.error == null)
+            //        {
+            //            errorStatusList.Add(0);
+            //        }
+            //        else
+            //        {
+            //            errorStatusList.Add(1);
+            //        }
+            //        //状态相同就添加数组
+            //        if(packageStatus == status)
+            //        {
+            //            PIDList.Add(one);
+
+            //            statusList.Add(packageStatus);
+
+            //            nameList.Add(packageInfo.dest.name);
+
+            //            phoneList.Add(packageInfo.dest.phone);
+
+            //            timeList.Add(packageInfo.package.time);
+            //        }
+            //        continue;
+            //    }
+            //    else if(searchWithName != null)
+            //    {
+            //        if(packageInfo.dest.name.ToString().StartsWith(searchWithName))
+            //        {
+            //            if (packageInfo.error == null)
+            //            {
+            //                errorStatusList.Add(0);
+            //            }
+            //            else
+            //            {
+            //                errorStatusList.Add(1);
+            //            }
+            //            PIDList.Add(one);
+
+            //            statusList.Add(packageInfo.package.status);
+
+            //            nameList.Add(packageInfo.dest.name);
+
+            //            phoneList.Add(packageInfo.dest.phone);
+
+            //            timeList.Add(packageInfo.package.time);
+            //        }
+            //        continue;
+            //    }
+            //    else if(searchWithPhone != null)
+            //    {
+            //        if (packageInfo.dest.phone.ToString().StartsWith(searchWithPhone))
+            //        {
+            //            if (packageInfo.error == null)
+            //            {
+            //                errorStatusList.Add(0);
+            //            }
+            //            else
+            //            {
+            //                errorStatusList.Add(1);
+            //            }
+
+            //            PIDList.Add(one);
+
+            //            statusList.Add(packageInfo.package.status);
+
+            //            nameList.Add(packageInfo.dest.name);
+
+            //            phoneList.Add(packageInfo.dest.phone);
+
+            //            timeList.Add(packageInfo.package.time);
+            //        }
+            //        continue;
+            //    }
+            //    else if (searchWithPID != null)
+            //    {
+            //        if (one.ToString().ToString().StartsWith(searchWithPID))
+            //        {
+            //            if (packageInfo.error == null)
+            //            {
+            //                errorStatusList.Add(0);
+            //            }
+            //            else
+            //            {
+            //                errorStatusList.Add(1);
+            //            }
+            //            PIDList.Add(one);
+
+            //            statusList.Add(packageInfo.package.status);
+
+            //            nameList.Add(packageInfo.dest.name);
+
+            //            phoneList.Add(packageInfo.dest.phone);
+
+            //            timeList.Add(packageInfo.package.time);
+            //        }
+            //        continue;
+            //    }
+            //    else if (searchWithTime != null)
+            //    {
+                    
+            //        if (packageInfo.package.time.ToString().StartsWith(searchWithTime))
+            //        {
+            //            if (packageInfo.error == null)
+            //            {
+            //                errorStatusList.Add(0);
+            //            }
+            //            else
+            //            {
+            //                errorStatusList.Add(1);
+            //            }
+            //            PIDList.Add(one);
+
+            //            statusList.Add(packageInfo.package.status);
+
+            //            nameList.Add(packageInfo.dest.name);
+
+            //            phoneList.Add(packageInfo.dest.phone);
+
+            //            timeList.Add(packageInfo.package.time);
+            //        }
+            //        continue;
+            //    }
+            //    else
+            //    {
+            //        if (packageInfo.error == null)
+            //        {
+            //            errorStatusList.Add(0);
+            //        }
+            //        else
+            //        {
+            //            errorStatusList.Add(1);
+            //        }
+            //        PIDList.Add(one);
+
+            //        statusList.Add(packageInfo.package.status);
+
+            //        nameList.Add(packageInfo.dest.name);
+
+            //        phoneList.Add(packageInfo.dest.phone);
+
+            //        timeList.Add(packageInfo.package.time);
+
+            //        continue;
+            //    }
+                
+            //}
+
+            //ViewBag.errorStatusList = errorStatusList;
+            //ViewBag.packagesID = PIDList;
+            //ViewBag.nameList = nameList;
+            //ViewBag.phoneList = phoneList;
+            //ViewBag.timeList = timeList;
+            //ViewBag.statusList = statusList;
+            //return View();
         }
         
     }
