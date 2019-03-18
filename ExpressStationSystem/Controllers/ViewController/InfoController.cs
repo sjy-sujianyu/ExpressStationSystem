15a37989abbceb55a4f15be6fbc7459cc8884e71
Nickwuyihong <2625454060@qq.com>
2019-03-18 17:05:41 +0800

    修改提成

--- ExpressStationSystem/Controllers/ViewController/InfoController.cs
+++ ExpressStationSystem/Controllers/ViewController/InfoController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,7 +11,21 @@ namespace ExpressStationSystem.Controllers.ViewController
     public class InfoController : Controller
     {
         // GET: Info
-        public ActionResult AllInfo()
+        public ActionResult AllInfo(string date1,string date2)
+        {
+            if (date1 == null)
+            {
+                date1 = "1999-11-11";
+                date2 = "2019-03-18";
+            }
+            var number = new QueryController().GetStatistic(Convert.ToDateTime(date1),Convert.ToDateTime(date2));
+            var jsonStr = JsonConvert.SerializeObject(number);
+            JObject jo = (JObject)JsonConvert.DeserializeObject(jsonStr);
+            ViewBag.number = jo;
+            return View();
+        }
+
+        public ActionResult ModifyCommision()
         {
             return View();
         }
