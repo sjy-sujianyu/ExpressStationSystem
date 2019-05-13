8ade6fc863b7ce07bbd436b989fe5c358f5037be
Nickwuyihong <2625454060@qq.com>
2019-05-13 18:22:39 +0800

    1

--- ExpressStationSystem/Models/Global.cs
+++ ExpressStationSystem/Models/Global.cs
@@ -182,7 +182,7 @@ namespace ExpressStationSystem.Models
         public static string GetUserToken(string account,string password)
         {
             FormsAuthenticationTicket ticket = new FormsAuthenticationTicket(0, "cookie", DateTime.Now,
-                        DateTime.Now.AddHours(1), false, string.Format("{0}&{1}", account, password),
+                        DateTime.Now.AddHours(24), true, string.Format("{0}&{1}", account, password),
                         FormsAuthentication.FormsCookiePath);
             return FormsAuthentication.Encrypt(ticket);
         }
@@ -200,7 +200,7 @@ namespace ExpressStationSystem.Models
                 return null;
             }
             FormsAuthenticationTicket ticket = new FormsAuthenticationTicket(0, "cookie", DateTime.Now,
-                        DateTime.Now.AddHours(1), false, string.Format("{0}&{1}", account, password),
+                        DateTime.Now.AddHours(24), true, string.Format("{0}&{1}", account, password),
                         FormsAuthentication.FormsCookiePath);
             return FormsAuthentication.Encrypt(ticket);
             
@@ -230,7 +230,7 @@ namespace ExpressStationSystem.Models
                 }
                 return true;
             }
-            catch
+            catch(Exception e)
             {
                 return false;
             }
